fedf5682523cb545eeb259ab088862f238561804
Jeanette93Cph <jeanette.93@live.dk>
2025-05-17 13:42:03 +0200

    minor
    minor

---
+++ MP3/Streamlit/pages/main.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+import pandas as pd
+
+st.title("MP3 ")
+st.write("Machine learning for analysis and prediction")
+
+
+
+
+# Kør følgende i terminalen for at få streamlit til at køre:
+
+# pip install streamlit
+# streamlit run main.py
